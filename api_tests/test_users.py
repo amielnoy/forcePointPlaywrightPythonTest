a041c2690f1b795cfaacb1849b9290ab440c978c
amielpeled <amielnoy@gmail.com>
2025-07-09 19:01:23 +0300

    add users api tests

--- api_tests/test_users.py
+++ api_tests/test_users.py
@@ -8,28 +8,3 @@ def test_get_user_by_id(api):
     assert data["id"] == 1
     assert "username" in data
 
-def test_create_post(api):
-    payload = {
-        "userId": 1,
-        "title": "New test post",
-        "body": "This is a body of the test post"
-    }
-    response = api.post("posts", data=payload)
-    assert response.status_code == ApiHttpConstants.CREATED
-    data = response.json()
-    assert data["title"] == payload["title"],"data <> payload!"
-
-def test_update_post(api):
-    payload = {
-        "id": 1,
-        "title": "Updated title",
-        "body": "Updated body",
-        "userId": 1
-    }
-    response = api.put("posts/1", data=payload)
-    assert response.status_code == ApiHttpConstants.OK
-    assert response.json()["title"] == "Updated title"
-
-def test_delete_post(api):
-    response = api.delete("posts/1")
-    assert response.status_code == ApiHttpConstants.OK

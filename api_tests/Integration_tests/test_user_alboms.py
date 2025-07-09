53c4db3af3a89be7894eea39d2fbdd01443b219a
amielpeled <amielnoy@gmail.com>
2025-07-09 18:59:54 +0300

    add integration tests

---
+++ api_tests/Integration_tests/test_user_alboms.py
@@ -0,0 +1,16 @@
+from globals import ApiHttpConstants
+
+
+def test_get_posts_count(api):
+    response = api.get("users/1/albums")
+    assert response.status_code == ApiHttpConstants.OK
+    data = response.json()
+    assert len(data) == 10
+
+def test_get_user_albom_number_2(api):
+    response = api.get("users/1/albums")
+    assert response.status_code == ApiHttpConstants.OK
+    data = response.json()
+    assert data[2]['userId'] == 1,"error in user id,expected 3"
+    assert data[2]['id'] == 3,"error is user id,expected 3"
+    assert data[2]['title'] == "omnis laborum odio"

c628933a646bd0fa28617d6e6b987945c8347be1
amielpeled <amielnoy@gmail.com>
2025-07-09 22:38:45 +0300

    add test_get_user_by_id2 test

--- api_tests/test_users.py
+++ api_tests/test_users.py
@@ -8,3 +8,10 @@ def test_get_user_by_id(api):
     assert data["id"] == 1
     assert "username" in data
 
+def test_get_user_by_id2(api):
+    response = api.get("users/2")
+    assert response.status_code == ApiHttpConstants.OK
+    data = response.json()
+    assert data["id"] == 2
+    assert "username" in data
+
